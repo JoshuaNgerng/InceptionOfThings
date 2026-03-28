2f7b0b356eff7b6aeaec02269dc889f17be637b3
JoshuaNgerng <joshuangerng@gmail.com>
2026-03-28 18:39:12 +0800

    adjust namespace for bonus

--- bonus/scripts/startup.sh
+++ bonus/scripts/startup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#!/bin/bash
-
 set -e
 
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
@@ -15,7 +13,7 @@ kubectl config use-context bonus
 
 echo "Creating namespace..."
 # 2. Namespace (already done)
-kubectl create namespace apps || true
+kubectl create namespace gitlab || true
 
 # 3. Config  
 kubectl apply -f ./confs/configmap.yaml
@@ -25,13 +23,13 @@ kubectl apply -f ./confs/configmap.yaml
 # 4. Postgres (StatefulSet)
 kubectl apply -f ./confs/postgres-service.yaml
 kubectl apply -f ./confs/postgres-statefulset.yaml
-kubectl rollout status statefulset/postgres -n apps --timeout=120s
+kubectl rollout status statefulset/postgres -n gitlab --timeout=120s
 
 # 5. Redis 
 kubectl apply -f ./confs/redis-deployment.yaml
-kubectl wait --for=condition=available deployment/redis -n apps --timeout=120s
+kubectl wait --for=condition=available deployment/redis -n gitlab --timeout=120s
 
-# 6. Services (so apps can connect)
+# 6. Services (so gitlab can connect)
 kubectl apply -f ./confs/services.yaml
 
 # 7. Application LAST
@@ -46,10 +44,10 @@ echo "Cluster status:"
 kubectl get nodes
 
 echo "Services:"
-kubectl get svc -n apps
+kubectl get svc -n gitlab
 
 echo "PVC:"
-kubectl get pvc -n apps
+kubectl get pvc -n gitlab
 
 echo "Pods:"
-kubectl get pods -n apps -o wide
+kubectl get pods -n gitlab -o wide
