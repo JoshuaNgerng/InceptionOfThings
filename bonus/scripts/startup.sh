2b8348a06b76966524714cacf87def848da41db2
JoshuaNgerng <joshuangerng@gmail.com>
2026-03-28 19:14:46 +0800

    add debug to bonus startup script

--- bonus/scripts/startup.sh
+++ bonus/scripts/startup.sh
@@ -15,27 +15,33 @@ echo "Creating namespace..."
 # 2. Namespace (already done)
 kubectl create namespace gitlab || true
 
-# 3. Config  
+# 3. Config 
+echo "Applying config map" 
 kubectl apply -f ./confs/configmap.yaml
 # if have secret
 # kubectl apply -f /vagrant/confs/secret.yaml
 
 # 4. Postgres (StatefulSet)
+echo "Applying postgres "
 kubectl apply -f ./confs/postgres-service.yaml
 kubectl apply -f ./confs/postgres-statefulset.yaml
 kubectl rollout status statefulset/postgres -n gitlab --timeout=120s
 
 # 5. Redis 
+echo "Applying redis"
 kubectl apply -f ./confs/redis-deployment.yaml
 kubectl wait --for=condition=available deployment/redis -n gitlab --timeout=120s
 
 # 6. Services (so gitlab can connect)
+echo "Applying service"
 kubectl apply -f ./confs/services.yaml
 
 # 7. Application LAST
+echo "Applying gitlab"
 kubectl apply -f ./confs/gitlab-deployment.yaml
 
 # 8. Ingress (optional, after service works)
+echo "Applying ingress"
 kubectl apply -f ./confs/ingress.yaml
 
 echo "Deployment finished."
